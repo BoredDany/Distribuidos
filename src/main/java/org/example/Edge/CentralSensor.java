2470a0fb0d1609410d03acaf77738c12c0637166
Santiago <or.santiago@javeriana.edu.co>
2024-03-30 18:06:01 -0500

    Intento de hacer probabilidades

--- src/main/java/org/example/Edge/CentralSensor.java
+++ src/main/java/org/example/Edge/CentralSensor.java
@@ -5,29 +5,39 @@ import org.zeromq.ZMQ;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CentralSensor {
     public static void main(String[] args) {
-        List<SensorHumo> sensoresHumo = new ArrayList<>();
 
+        Random random = new Random();
+
+        List<SensorHumo> sensoresHumo = new ArrayList<>();
+        String archivoConfigSH = "sensoresHumo.txt";
+        List<Double> probabilidadesSH ; // Falta asignar método para leer el archivo de configuracion
+
+        // Crear instancias de Sensores de Humo
         for (int i = 0; i < 10; i++) {
             SensorHumo sh = new SensorHumo();
             sensoresHumo.add(sh);
 
         }
 
+        // Crear instancias de Sensores de Humedad
         List<SensorHumedad> sensoresHumedad = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             SensorHumedad shu = new SensorHumedad();
             sensoresHumedad.add(shu);
         }
 
+        // Crear instancias de Sensores de Temperatura
         List<SensorTemperatura> sensoresTemperatura = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             SensorTemperatura st = new SensorTemperatura();
             sensoresTemperatura.add(st);
         }
 
+        // Aquí se realiza la comunicación con el proxy
         try (ZContext context = new ZContext()) {
             // Crear un socket para comunicarse con el proxy
             ZMQ.Socket socket = context.createSocket(ZMQ.REQ);
