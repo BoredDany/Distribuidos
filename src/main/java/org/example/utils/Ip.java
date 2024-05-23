2806fddf803c9883aa74fa05db3f9e3a822c2f96
BoredDany <martinezq.d@javeriana.edu.co>
2024-05-23 02:24:54 -0500

    correccion comunicacion en cloud

--- src/main/java/org/example/utils/Ip.java
+++ src/main/java/org/example/utils/Ip.java
@@ -25,35 +25,5 @@ public class Ip {
     public static final String PORT_SC_FOG = "5200";
     public static final String PORT_SC_CLOUD = "5300";
 
-    /*
-
-    //SENSORES
-    public static final String CENTRAL_SENSOR = "localhost";
-
-    //PROXY
-    public static final String PROXY_PRINCIPAL = "localhost";
-    public static final String PROXY_SECUNDARIO = "10.43.100.223";
-
-    //HEALTH CHECKER
-    public static final String HEALTH_CHECKER = "10.43.100.223";
-
-    //SISTEMAS DE CALIDAD
-    public static final String SC_FOG = "localhost";
-    public static final String SC_EDGE = "localhost";
-    public static final String SC_CLOUD = "localhost";
-
-    //CLOUD
-    public static final String CLOUD = "localhost";
-
-
-    //PORTS
-    public static final String PORT_SENSOR_ASPERSOR = "5000";
-    public static final String PORT_SENSOR_PROXY = "5555";
-    public static final String PORT_PROXY_CLOUD = "5556";
-    public static final String PORT_SC_EDGE = "5100";
-    public static final String PORT_SC_FOG = "5200";
-    public static final String PORT_SC_CLOUD = "5300";
-    */
-
 }
 
